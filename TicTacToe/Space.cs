bb44d939ea21942cdbd5819f8aab2e503856a456
milolucero <camiloluceroe@gmail.com>
2022-05-07 17:26:19 -0500

    Add Space class fields, constructors, and methods.

--- TicTacToe/Space.cs
+++ TicTacToe/Space.cs
@@ -8,5 +8,34 @@ namespace TicTacToe
 {
     internal class Space
     {
+        private readonly Position position;
+        private bool isOccupied;
+        private Player? occupant;
+
+        public Space(Position position, Player? occupant)
+        {
+            this.position = position;
+            this.occupant = occupant;
+        }
+
+        public Space(Position position) : this(position, null)
+        {
+
+        }
+
+        public Position GetPosition()
+        {
+            return position;
+        }
+
+        public Player? GetOccupant()
+        {
+            return occupant;
+        }
+
+        public void SetOccupant(Player? occupant)
+        {
+            this.occupant = occupant;
+        }
     }
 }
