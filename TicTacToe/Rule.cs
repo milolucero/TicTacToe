1587f3b2bd1d01be61c2581c0891b2ca480859a5
milolucero <camiloluceroe@gmail.com>
2022-05-07 19:23:58 -0500

    Add a class for the rules and limits of the game.

---
+++ TicTacToe/Rule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class Rule
+    {
+        private static (int height, int width) boardDimensions = (3, 3);
+        private static int maxNumberOfPlayers = 2;
+        private static int consecutiveShapesToWin = 3;
+
+        public static (int height, int width) GetBoardDimensions()
+        {
+            return boardDimensions;
+        }
+
+        public static int GetMaxNumberOfPlayers()
+        {
+            return maxNumberOfPlayers;
+        }
+
+        public static int GetConsecutiveShapesToWin()
+        {
+            return consecutiveShapesToWin;
+        }
+    }
+}
